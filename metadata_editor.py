a207d86d64822723a427a3a8bda57b8223dd7e51
Razumovskiy Nikolay <n.razumovskiy@NB0300.local>
2022-06-09 23:32:24 +0300

    Add support for pptx

--- metadata_editor.py
+++ metadata_editor.py
@@ -1,7 +1,10 @@
 from docx import Document
+from pptx import Presentation
+
 from datetime import datetime
 import time
 import os
+
 
 def edit_doc(file_path: str, out_path: str, created_datetime:datetime or None=None, last_modified_datetime: datetime or None=None, author: str or None=None):
     document = Document(file_path)
@@ -16,20 +19,36 @@ def edit_doc(file_path: str, out_path: str, created_datetime:datetime or None=No
         mod_time = time.mktime(last_modified_datetime.timetuple())
         os.utime(out_path, (mod_time, mod_time))
 
+def edit_pptx(file_path: str, out_path: str, created_datetime:datetime or None=None, last_modified_datetime: datetime or None=None, author: str or None=None):
+    prs = Presentation(file_path)
+    if created_datetime is not None:
+        prs.core_properties.created = created_datetime
+    if last_modified_datetime is not None:
+        prs.core_properties.modified = last_modified_datetime
+    if author is not None:
+        prs.core_properties.author = author
+    prs.save(out_path)
+    if last_modified_datetime is not None:
+        mod_time = time.mktime(last_modified_datetime.timetuple())
+        os.utime(out_path, (mod_time, mod_time))
+    
+
 def process_dir(root_dir: str, out_dir: str, created_datetime:datetime or None=None, last_modified_datetime: datetime or None=None, author: str or None=None):
     if not os.path.isdir(out_dir):
         os.mkdir(out_dir)
     files = os.listdir(root_dir)
     for c_file in files:
         if os.path.isfile(root_dir + c_file):
-            process_file(root_dir + c_file, out_dir + c_file)
+            process_file(root_dir + c_file, out_dir + c_file, created_datetime, last_modified_datetime, author)
         else:
-            process_dir(root_dir + c_file + '/', out_dir + c_file + '/')
+            process_dir(root_dir + c_file + '/', out_dir + c_file + '/', created_datetime, last_modified_datetime, author)
 
 def process_file(root_dir: str, out_dir: str, created_datetime:datetime or None=None, last_modified_datetime: datetime or None=None, author: str or None=None):
     if root_dir.endswith('.doc') or root_dir.endswith('.docx'):
         edit_doc(root_dir, out_dir, created_datetime, last_modified_datetime, author)
-c
+    elif root_dir.endswith('.pptx'):
+        edit_pptx(root_dir, out_dir, created_datetime, last_modified_datetime, author)
+
 def generate_datetime_for_docx_custom_logic():
     '''
     If you need custom logic to generate datetime, you can write that logic in this function
@@ -39,4 +58,5 @@ def generate_datetime_for_docx_custom_logic():
 if __name__ == "__main__":
     in_path = ""
     out_path = ""
-    process_dir(in_path, out_path)
+    date = datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
+    process_dir(in_path, out_path, date, date)
